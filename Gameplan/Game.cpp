111f61a25c0dee3531d2feb6549b0ad2e0bd326e
Eero <eero.kokkonen@hotmail.com>
2022-06-10 19:16:18 +0300

    Lisää suunnitelmia

--- Gameplan/Game.cpp
+++ Gameplan/Game.cpp
@@ -4,12 +4,53 @@
 Game::Game(Application* _application)
 {
 	application = _application;
+	// Luo pelaajan, pelipöydän ja taustakuvan
+
 }
 
 void Game::update()
 {
+	//drawBackground();
+	switch(state)
+	{
+	case GameState::STATE_LOBBY:
+		if (playerList.size() >= requiredPlayers)
+			state = GameState::STATE_GAME;
+
+		break;
+	case GameState::STATE_GAME:
+		for (Player* player : playerList)
+		{
+			if (player->data.isTurn)
+				;//updateTable(player);
+		}
+
+		break;
+	case GameState::STATE_ENDMENU:
+		// Voittajan ilmoitus, pelin uusinta, (pelin vaihto), 
+		break;
+	}
 }
 
 void Game::shutdown()
 {
+}
+
+void Game::updatePlayersCallback(gameData& _data)
+{
+	// Ensimmäinen viesti on aina tälle pelaajalle
+	// Viestistä otetaan talteen pelaajan numero
+	if (playerList.size() < requiredPlayers)
+		playerList.push_back(new Player);
+	for (Player* player : playerList)
+	{
+		if (_data.playerNumber == player->data.playerNumber)
+			player->data = _data;
+	}
+	
+}
+
+void Game::waitingPlayers()
+{
+	//
 }
